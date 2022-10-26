463af8df88d346bfaaaf1eedfdc3596a17878b8b
Kibahop <petri.lammi@puppeteers.net>
2022-10-27 00:31:46 +0300

    WIP: some simplification and reorganization
    Signed-off-by: Kibahop <petri.lammi@puppeteers.net>

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,4 +3,65 @@
 #
 class ipacerts::config {
   assert_private()
+
+  # XXX: in progress
+
+  /*  
+  
+  # Add the cas to ipa and apache nss databases
+  # NOTE: the order is critical in your sourcehash
+  ipa_cas { $ipacerts::sourcehash:
+    ensure => 'present',
+    source => $ipacerts::sourcehash,
+   }
+
+   # Add the sertificate and the key to the nss database
+   webui_cert_and_key { 'wildcard.openvpn.in':
+     ensure      => 'present',
+     key_source  => "${ipacerts::certdir}/${ipacerts::keyname}",
+     cert_source => "${ipacerts::certdir}/${ipacerts::certname}",
+     nss_pwfile  =>  "${certdir}/nss-password.txt", 
+   }
+
+  exec { "add_cert_${title}":
+    path      => ['/usr/bin'],
+    command   => "certutil -d ${certdir} -A -n '${nickname}' -t '${trustargs}' -a -i ${cert}",
+    unless    => "certutil -d ${certdir} -L -n '${nickname}'",
+    logoutput => true,
+    require   => [
+      Nsstools::Create[$certdir],
+      Class['nsstools'],
+    ],
+  }
+  
+  exec {"generate_pkcs12_${title}":
+    command   => "/usr/bin/openssl pkcs12 -export -in ${cert} -inkey ${key} -password 'file:${certdir}/nss-password.txt' -out '${certdir}/${pkcs12_name}' -name '${nickname}'",
+    creates   => "${certdir}/${pkcs12_name}",
+    subscribe => File["${certdir}/nss-password.txt"],
+    require   => [
+      Nsstools::Create[$certdir],
+      Class['nsstools'],
+    ],
+  }
+
+  exec { "add_pkcs12_${title}":
+    path      => ['/usr/bin'],
+    command   => "pk12util -d ${certdir} -i ${certdir}/${pkcs12_name} -w ${certdir}/nss-password.txt -k ${certdir}/nss-password.txt",
+    unless    => "certutil -d ${certdir} -L -n '${nickname}'",
+    logoutput => true,
+    require   => [
+      Exec["generate_pkcs12_${title}"],
+      Nsstools::Create[$certdir],
+      Class['nsstools'],
+    ],
+  }
+
+  file_line { 'Ensure cert nickname':
+    path    => '/etc/httpd/conf.d/nss.conf',
+    replace => true,
+    line    => 'NSSNickname wildcard.openvpn.in',
+    match   => '^NSSNickname.*$',
+    notify  => Service['httpd'],
+  }
+ */
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -2,16 +2,16 @@
 # @summary Manage 3rd party certificates in FreeIPA
 #
 # @param chainhash
-#  A hash with alphabetically sorted keys of 
+#  A hash with numerically sorted keys of 
 #  valid file uris to retrieve 
 #  CA certificates in correct order
 #
 #  example:
 # 
 # $chainhash = {
-#  'c' => 'http://path/cert3.crt',
-#  'b' => 'http://path/cert2.crt',
-#  'a' => 'file:///path/cert1.crt'
+#  '3' => 'http://path/cert3.crt',
+#  '2' => 'http://path/cert2.crt',
+#  '1' => 'file:///path/cert1.crt'
 # }
 class ipacerts (
   Hash $chainhash,
@@ -28,17 +28,10 @@ class ipacerts (
 ) {
 
   contain 'ipacerts::initialize'
-  contain 'ipacerts::install_cacerts'
-  contain 'ipacerts::install_cert_and_key'
-  contain 'ipacerts::validate'
+  contain 'ipacerts::install'
   contain 'ipacerts::config'
 
   Class['ipacerts::initialize']
-  -> Class['ipacerts::install_cacerts']
-  -> Class['ipacerts::install_cert_and_key']
-  -> Class['ipacerts::validate']
+  -> Class['ipacerts::install']
   -> Class['ipacerts::config']
-
-  Class['ipacerts::install_cacerts']~>Class['ipacerts::validate']
-  Class['ipacerts::install_cert_and_key']~>Class['ipacerts::validate']
 }
