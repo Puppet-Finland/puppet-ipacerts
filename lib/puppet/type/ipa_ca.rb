91aa71d4e1e20f76a0da5b95cae0644cd31cb1e1
Petri Lammi <petri.lammi@puppeteers.net>
2022-12-12 13:43:23 +0200

    WIP: update ipa_ca type
    Signed-off-by: Petri Lammi <petri.lammi@puppeteers.net>

--- lib/puppet/type/ipa_ca.rb
+++ lib/puppet/type/ipa_ca.rb
@@ -1,37 +1,51 @@
 # coding: utf-8
-Puppet::Type.newtype(:ipa_ca) do
-  @doc = 'Manage 3rd party CA certificate in FreeIPA'
-  ensurable
+module Puppet
+  Type.newtype(:ipa_ca) do
 
-  newparam(:name, :namevar => true) do
-    desc 'Name of the CA cert'
-  end
-  
-  newproperty(:file) do
-    desc 'File for the CA cert'
-    validate do |value|
-      raise("%s does not exist" % value) unless File.file?(value)
+    @doc = 'Manage 3rd party CA certificate in FreeIPA'
+
+    ensurable do
+
+      desc 'Create or remove the CA cert.'
+      
+      newvalue(:present) do
+        provider.create
+      end
+      
+      newvalue(:absent) do
+        provider.destroy
+      end
+
+      defaultto :present
     end
-  end
 
-  newparam(:nickname_by) do
-    desc 'Nickname base in NSS database for the certificate'
-    defaultto :subject
-    newvalues(:subject, :filename)
-  end
-
-  newparam(:order) do
-    desc 'Ca order in the chain'
-    validate do |value|
-      raise("%s is not an integer" % value) unless value.is_a?(Integer)
+    newparam(:name, namevar: true) do
+      desc 'The CA cert name'
+      validate do |value|
+        raise("%s is not an string" % value) unless value.is_a?(String)
+      end
     end
-  end
-
-  newparam(:trustargs) do
-    Puppet.debug(:trustargs)
-    desc 'Trustargs'
-    validate do |value|
-      raise("%s does not exist" % value) unless value.is_a?(String)
+    
+    newparam(:filepath) do
+      desc 'Filepath for the CA cert'
+      validate do |value|
+        raise("%s is not an absolute path" % value) unless Puppet::Util.absolute_path?(value)
+      end
+    end
+    
+    newparam(:order) do
+      desc 'Ca order in the chain'
+      validate do |value|
+        raise("%s is not an integer" % value) unless value.is_a?(Integer)
+      end
+    end
+    
+    newparam(:trustargs) do
+      Puppet.debug(:trustargs)
+      desc 'Trustargs'
+      validate do |value|
+        raise("%s does not exist" % value) unless value.is_a?(String)
+      end
     end
   end
 end
